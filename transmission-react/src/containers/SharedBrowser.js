c57a6b7ab476d08cb974efb94be7d500f350d787
nk2303 <ngankhong03@gmail.com>
2020-07-29 23:27:41 -0700

    adding writting form into shared browser

--- transmission-react/src/containers/SharedBrowser.js
+++ transmission-react/src/containers/SharedBrowser.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form'
 import { api } from '../services/api';
 
 const SharedBrowser = () => {
 
     return (
-        <div>
-            Welcome to Share
-        </div>
+        <Form.Group controlId="formControlTextArea">
+            <Form.Label>Shared text</Form.Label>
+            <Form.Control 
+                as="textarea" 
+                rows="30" 
+                placeholder="Write or paste text here. Anyone you share with will see your text as it is typed..."
+            />
+        </Form.Group>
     )
 }
 
