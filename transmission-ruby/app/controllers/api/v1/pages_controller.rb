1009b85ba7fdd32d507f892ea61526ccaef2f8ea
nk2303 <ngankhong03@gmail.com>
2020-07-30 00:05:52 -0700

    write update function for page

--- transmission-ruby/app/controllers/api/v1/pages_controller.rb
+++ transmission-ruby/app/controllers/api/v1/pages_controller.rb
@@ -4,19 +4,33 @@ class Api::V1::PagesController < ApplicationController
         @pages = Page.all
         render json: @pages, status: :ok
     end
+
     def show
         page = Page.find(params[:id])
         render json: page
     end
+
     def create
         page = Page.create(page_params)
         render json: {page: PageSerializer.new(page)}
     end
+
+    def update
+        page = Page.find(params[:url_key])
+        page.update(page_params)
+        if page.valid? 
+            render json: { page: PageSerializer.new(page) }
+        else
+            render json: { error: 'failed to update page' }, status: :not_acceptable
+        end
+    end
+
     def destroy
         page = Page.find(params[:id])  
         page.destroy
         render json: {message: "page successfully deleted"}
     end
+
     private
     def page_params
         params.require(:page).permit(:url_key, :content)
