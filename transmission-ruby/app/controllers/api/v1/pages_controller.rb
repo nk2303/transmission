4881bf896c82de6bfcd3d79ff778582c9b676947
nk2303 <ngankhong03@gmail.com>
2020-07-31 14:13:34 -0700

    first heroku commit

--- transmission-ruby/app/controllers/api/v1/pages_controller.rb
+++ transmission-ruby/app/controllers/api/v1/pages_controller.rb
@@ -1,17 +1,19 @@
 class Api::V1::PagesController < ApplicationController
     def index
         # Can return array of url_keys only
-        @pages = Page.all
+        @pages = Page.all.map{|p| p.url_key}
         render json: @pages, status: :ok
     end
 
     def show
+        puts "GET TO SHOW METHOD"
         page = Page.find(params[:id])
         render json: page
     end
 
     # When a browser ask for joinPage method, it means the user is joining the Page and can see all texts. 
     def joinPage
+        puts "GET TO JOIN PAGE METHOD"
         page = Page.all.find{|p| p.url_key == params[:url_key]}
         render json: page
     end
@@ -22,10 +24,11 @@ class Api::V1::PagesController < ApplicationController
     end
 
     def update
+        puts "GET TO UPDATE PAGE METHOD"
         page = Page.all.find{|p| p.url_key == params[:url_key]}
         page.update(page_params)
         if page.valid? 
-            ActionCable.server.broadcast 'page',
+            ActionCable.server.broadcast 'pages',
                 page: page.content
                 render json: { page: PageSerializer.new(page) }
             head :ok
